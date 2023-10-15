354444fbe45d70619880b14d7750ea4b96ff93c3
Marco Dondo <dondo.marco.itsv@gmail.com>
2023-10-15 17:02:29 +0200

    Update infrastructure.tf

--- Inf/TerraformInf/infrastructure.tf
+++ Inf/TerraformInf/infrastructure.tf
@@ -34,24 +34,10 @@ resource "awslightsail_container_service" "my_container_service" {
   is_disabled = false
 }
 
-resource "awslightsail_database" "my_db" {
-  name                 = "my-db-1"
-  availability_zone    = "eu-west-1a"
-  master_username      = "myusername"
-  master_database_name = "mydatabase"
-  master_password      = "mypassword123!"
-  blueprint_id         = "mysql_8_0"
-  bundle_id            = "micro_1_0"
-}
-
 output "public_ip" {
   value = aws_lightsail_static_ip.my_static_ip.ip_address
 }
 
 output "container_service_domain" {
   value = awslightsail_container_service.my_container_service.url
-}
-
-output "database_endpoint" {
-  value = awslightsail_database.my_db.master_endpoint_address
 }
