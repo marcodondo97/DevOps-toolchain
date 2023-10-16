8a1385ec9af23a131c3cdba8b165dd338e57b224
Marco Dondo <dondo.marco.itsv@gmail.com>
2023-10-16 10:16:26 +0200

    Update service1.py

--- App/Service1/service1.py
+++ App/Service1/service1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def service1():
-    return jsonify({'message': 'Response from service 1!'})
+    return jsonify({'message': 'Response from service 1'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
