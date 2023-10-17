9a69a146555452ee93978f34618b1e97e25ca83d
marcodondo97 <marco.dondo97@gmail.com>
2023-10-17 20:13:42 +0200

    PD-7 bugs fixed in test_service1

--- Test/test_service1.py
+++ Test/test_service1.py
@@ -4,14 +4,14 @@ import requests
 class TestAPIEndpoint(unittest.TestCase):
     def test_api_endpoint(self):
         endpoint_url = 'http://localhost:5001'
-#test
+
         try:
             response = requests.get(endpoint_url)
             if response.status_code == 200:
                 result = "OK"
             else:
                 result = "KO"
-                
+                #pi
         except Exception as e:
             result = "KO"   
         print(result)
