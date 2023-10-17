2d0740301522d01bbb733d1b8e1c5bfebb81cae5
marcodondo97 <marco.dondo97@gmail.com>
2023-10-17 20:23:56 +0200

    PD-9 bugs fixed in service_test1

--- Test/test_service1.py
+++ Test/test_service1.py
@@ -4,7 +4,7 @@ import requests
 class TestAPIEndpoint(unittest.TestCase):
     def test_api_endpoint(self):
         endpoint_url = 'http://localhost:5001'
-
+#test
         try:
             response = requests.get(endpoint_url)
             if response.status_code == 200:
